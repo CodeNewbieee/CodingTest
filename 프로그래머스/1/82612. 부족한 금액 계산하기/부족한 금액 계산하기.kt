0ffaeb15ce4b0d57eeeab89cae995dba1c2e9dd4
CodeNewbieee <150916425+CodeNewbieee@users.noreply.github.com>
2023-12-08 10:45:11 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.07 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
@@ -1,15 +1,11 @@
 class Solution {
     fun solution(price: Int, money: Int, count: Int): Long {
         var answer : Long = 0
-        var totalPrice : Long = 0
         //놀이기구 총 이용 금액
         for(i in 1..count) {
-            totalPrice += price*i
+            answer += price*i
         }
         
-        if(totalPrice <= money) answer = 0
-        else answer = totalPrice - money
-        
-        return answer
+        return if(answer <= money) 0 else answer-money
     }
 }
