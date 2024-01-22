0aaf212f763ea8d6baa9d22fd4cc4cc4d0227dab
CodeNewbieee <150916425+CodeNewbieee@users.noreply.github.com>
2024-01-22 09:15:48 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 13.34 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(s: String): String {
+        var answer = s.split(" ").joinToString(" ") {
+            it.mapIndexed { i, c ->
+            when(i % 2) {
+                0 -> c.toUpperCase()
+            else -> c.toLowerCase()
+            }
+            }.joinToString("")
+        }
+        return answer
+    }
+}
